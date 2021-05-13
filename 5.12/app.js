fdb431cade44a93cca7c47893a9ba038adcb5f6d
zuokangxin <226730448@qq.com>
2021-05-13 11:28:53 +0800

    5.12

--- 5.12/app.js
+++ 5.12/app.js
@@ -9,9 +9,9 @@ var connection = mysql.createConnection({
 connection.connect();    //建立链接
  
 
-let sql = "select * from p_users limit 10"
+let sql = "select * from p_users order by user_id desc limit 10"
 connection.query(sql, function (error, results, fields) {
-  console.log(error)
+  // console.log(error)
   console.log(results)
 });
  
